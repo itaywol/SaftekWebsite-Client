283e5fad4a2f8e379570ae145af6b65f93899a3a
itaywol <wo.itay@gmail.com>
2019-03-14 09:35:00 +0200

    navbar implementation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,21 @@
 import * as React from "react";
-import Navbar from "./components/Navbar";
-import { Image }from "react-bootstrap";
+import Navibar from "./components/Navibar";
+import { Image } from "react-bootstrap";
 import "./../assets/scss/App.scss";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 
-export interface AppProps {
-}
+export interface AppProps {}
 
 export default class App extends React.Component<AppProps, undefined> {
-    render() {
-        return (
-            <div className="app">
-                <Navbar />
-                <h1>Hello World!</h1>
-                <Image src={reactLogo} className="w-100 r-logo"/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <Navibar items={["Home", "About"]} className="container" />
+        <div className="cross-line" />
+        <h1>Hello World!</h1>
+        <Image src={reactLogo} className="w-100 r-logo" />
+      </div>
+    );
+  }
 }
